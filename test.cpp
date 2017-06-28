063ea5841f118f7d5289313a061ae0a1bc429803
samrtdan <1227860132@qq.com>
2017-06-27 21:23:47 -0700

    test.cpp

---
+++ test.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+    > File Name: test.cpp
+    > Author: huangjunjie
+    > Mail: huangjunjie@douyu.tv 
+    > Created Time: Tue 27 Jun 2017 09:19:38 PM PDT
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	std::cout<<"Hello, world!"<<std::endl;
+	return 0;
+}
